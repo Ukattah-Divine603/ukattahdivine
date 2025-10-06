2952e9075bf9cfa42bceda7eadf992a862c28c21
Ukattah-Divine603 <ukattahdivine603@gmail.com>
2025-10-06 19:47:03 +0100

    Added Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Aside from './components/Aside'
 import Home from './components/Home'
+import logo from './images/CODEVINE-LOGO-BLACK.png' // replace with your logo path
+
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000) // 2 seconds preloader
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="preloader">
+        <div className="spinner">
+          <img src={logo} alt="Logo" />
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       <Header />
       <div className="body-components">
-         <Aside />
-           <Home />
+        <Aside />
+        <Home />
       </div>
-     
-    
-
     </>
   )
 }
